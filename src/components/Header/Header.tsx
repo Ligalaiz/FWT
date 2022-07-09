da83f7d255ed15169c79704c423baab296598c00
Ligalaiz <slavakuz88@gmail.com>
2022-07-09 22:31:49 +0300

    feat: AG-1 implement Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import * as h from './Header.style';
+import Logo from '@assets/img/logo.svg';
+import Theme from '@assets/img/theme.svg';
+
+const Header = () => {
+  return (
+    <header>
+      <div css={h.headerContainer}>
+        <div css={h.headerLogo}>
+          <a css={h.headerLink} href="#top">
+            <Logo />
+          </a>
+        </div>
+        <div css={h.headerTheme}>
+          <Theme />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export { Header };
