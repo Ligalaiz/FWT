00df2249bad8543669084536a9fe8390a491087e
Ligalaiz <slavakuz88@gmail.com>
2022-07-09 14:02:41 +0300

    style: add colors variables

--- src/styles/variables.ts
+++ src/styles/variables.ts
@@ -4,8 +4,12 @@ interface IColor {
 
 export const color: IColor = {
   'white-color': '#ffffff',
-  'gray-color': '#dedede',
+  'white-color-light': '#ffffff30',
   'black-color': '#000000',
+  'black-color-light': '#00000030',
+  'gray-color': '#dedede',
+  'gray-color-light': '#E5E5E5',
+  'gray-color-light-x1': '#ffffffbf',
   'red-color': '#D00000',
   'blue-color': '#518fb4',
 };
