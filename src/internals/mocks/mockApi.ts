9b5f9d0b94111806bd8f33465a5ecf8eacb1cff7
Ligalaiz <slavakuz88@gmail.com>
2022-07-11 22:04:54 +0300

    add: AG-2 mock api data

---
+++ src/internals/mocks/mockApi.ts
@@ -0,0 +1,41 @@
+interface IOptions {
+  id: number;
+  name?: string;
+  location?: string;
+}
+
+export const options1: IOptions[] = [
+  { id: 1, name: 'Salvador daligf ddfgdfg dfgdfg dfgdfg' },
+  { id: 2, name: 'Vincent van gogh' },
+  { id: 3, name: 'Claude monet' },
+  { id: 4, name: 'Victor vasnetsov' },
+  { id: 5, name: 'Salvador dali' },
+  { id: 6, name: 'Vincent van gogh' },
+  { id: 7, name: 'Claude monet' },
+  { id: 8, name: 'Salvador dali' },
+  { id: 9, name: 'Vincent van gogh' },
+  { id: 10, name: 'Claude monet' },
+  { id: 1, name: 'Salvador dali' },
+  { id: 2, name: 'Vincent van gogh' },
+  { id: 3, name: 'Claude monet' },
+  { id: 4, name: 'Victor vasnetsov' },
+  { id: 5, name: 'Salvador dali' },
+  { id: 6, name: 'Vincent van gogh' },
+  { id: 7, name: 'Claude monet' },
+  { id: 8, name: 'Salvador dali' },
+  { id: 9, name: 'Vincent van gogh' },
+  { id: 10, name: 'Claude monet' },
+];
+
+export const options2: IOptions[] = [
+  { id: 1, location: 'The Tate Gallery' },
+  { id: 2, location: 'Santa Maria delle Grazie' },
+  { id: 3, location: 'Santa Maria delle Grazie' },
+  { id: 4, location: 'Santa Maria delle Grazie' },
+  { id: 5, location: 'The Tate Gallery' },
+  { id: 6, location: 'The Tate Gallery' },
+  { id: 7, location: 'The Tate Gallery' },
+  { id: 8, location: 'Santa Maria delle Grazie' },
+  { id: 9, location: 'The Tate Gallery' },
+  { id: 10, location: 'Santa Maria delle Grazie' },
+];
