0883af7d9a4f6b9c15db7e1652ce488dd32180eb
Ligalaiz <slavakuz88@gmail.com>
2022-07-14 19:53:25 +0300

    refactor: AG-3 hidden link for small screen

--- src/screens/Layout/Layout.tsx
+++ src/screens/Layout/Layout.tsx
@@ -1,11 +1,16 @@
 import React, { FC } from 'react';
 import { Outlet } from 'react-router-dom';
 import { GitLink } from '@components/GitLink';
+import useMatchMedia from 'use-match-media-hook';
+
+const queries = ['(min-width: 1366px)'];
 
 const Layout: FC = () => {
+  const [desctop] = useMatchMedia(queries);
+
   return (
     <>
-      <GitLink />
+      {desctop && <GitLink />}
       <Outlet />
     </>
   );
