35a6603c6395d86424ca53895771ba55b556b084
Ligalaiz <slavakuz88@gmail.com>
2022-07-14 19:55:46 +0300

    style: AG-3 change grid rows for media breakpoint

--- src/modules/Gallery/Gallery.style.ts
+++ src/modules/Gallery/Gallery.style.ts
@@ -3,8 +3,12 @@ import { media } from '@src/styles';
 
 export const main = css`
   padding-top: 11.3rem;
+  height: 100%;
+  display: grid;
+  grid-template-rows: 24.2rem 1fr 10rem;
 
   @media (${media.sm}) {
+    grid-template-rows: 6.1rem 1fr 10rem;
     padding-top: 13.3rem;
   }
 `;
