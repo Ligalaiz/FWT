9fb977dffc059a693a8fe1461a1492dbee1f6e96
Ligalaiz <slavakuz88@gmail.com>
2022-07-14 20:03:15 +0300

    style: AG-3 change grid template for several breakpoints

--- src/components/CardsList/CardsList.style.ts
+++ src/components/CardsList/CardsList.style.ts
@@ -3,22 +3,36 @@ import { media } from '@src/styles';
 
 export const cardsList = css`
   display: grid;
-  grid-row-gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
+  grid-gap: 2rem;
+  max-width: 28rem;
   padding: 2.9rem 0 3rem;
-  max-width: 74rem;
   margin-bottom: 3rem;
+  margin: 0 auto;
+
+  grid-template-columns: repeat(auto-fit, minmax(252px, 1fr));
+
+  @media (min-width: 578px) {
+    grid-template-columns: repeat(auto-fit, minmax(252px, 1fr));
+    max-width: 70rem;
+  }
 
   @media (${media.sm}) {
     grid-template-columns: repeat(auto-fit, minmax(280px, 1fr));
-    grid-gap: 2rem;
-    margin: 0 auto;
-    max-width: 98rem;
-    margin-bottom: 3.5rem;
   }
 
   @media (${media.md}) {
     grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
-    max-width: 116rem;
+    max-width: 94rem;
     margin-bottom: 4rem;
+  }
+
+  @media (min-width: 1025px) {
+    max-width: 1120rem;
+  }
+
+  @media (${media.lg}) {
+    grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
+    max-width: 1120rem;
   }
 `;
