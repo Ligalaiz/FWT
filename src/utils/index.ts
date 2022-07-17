10c8608c28efc0dd0e7e25389ba0f2b17280b0b0
Ligalaiz <slavakuz88@gmail.com>
2022-07-17 20:10:10 +0300

    refactor: AG-4 add imports

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,4 @@
 export { delay } from './delay.utils';
 export { serializeData } from './serializeData.utils';
 export { filterData } from './filterData.utils';
+export { createPagArray } from './createPagArray.utils';
