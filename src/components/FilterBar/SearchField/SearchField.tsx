891484a371a139f8fe55eb2f5d84252ce89930c5
Ligalaiz <slavakuz88@gmail.com>
2022-07-17 20:20:50 +0300

    refactor: AG-4 pass state from context

--- src/components/FilterBar/SearchField/SearchField.tsx
+++ src/components/FilterBar/SearchField/SearchField.tsx
@@ -1,17 +1,27 @@
-import React, { Dispatch, SetStateAction, ChangeEvent, KeyboardEvent } from 'react';
+import React, { Dispatch, SetStateAction, ChangeEvent, useContext, Context } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import * as s from './SearchField.style';
+import { IAppContext } from '@src/store/context';
 
 interface ISearchField {
   searchValue: string;
+  appcontext: Context<IAppContext>;
   setSearchValue: Dispatch<SetStateAction<string>>;
-  request: (e: KeyboardEvent<HTMLInputElement>) => void;
 }
-const SearchField = ({ searchValue, setSearchValue, request }: ISearchField) => {
+const SearchField = ({ searchValue, setSearchValue, appcontext }: ISearchField) => {
+  const {
+    searchActions: { setCurrentPage },
+  } = useContext(appcontext);
+  const { search } = useLocation();
+  const navigate = useNavigate();
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value } = e.target as typeof e.target & {
       value: string;
     };
     setSearchValue(value);
+    navigate(`/1${search}`);
+    setCurrentPage(1);
   };
 
   return (
@@ -19,7 +29,6 @@ const SearchField = ({ searchValue, setSearchValue, request }: ISearchField) => 
       <input
         value={searchValue}
         onChange={handleChange}
-        onKeyDown={request}
         css={s.searchField}
         type="text"
         name="name"
