353bdeeafc2da054d767b0794fc7e3b632363f9c
Ligalaiz <slavakuz88@gmail.com>
2022-07-17 20:23:23 +0300

    refactor: AG-4 remove props

--- src/components/FilterBar/FilterBar.tsx
+++ src/components/FilterBar/FilterBar.tsx
@@ -1,65 +1,29 @@
-import React, { KeyboardEvent, Dispatch, SetStateAction } from 'react';
+import React, { Context, useContext } from 'react';
 import * as f from './FilterBar.style';
 import { SearchField } from '@components/FilterBar/SearchField';
 import { SelectField } from '@components/FilterBar/SelectField';
 import { RangeField } from '@components/FilterBar/RangeField';
+import { IAppContext } from '@src/store/context';
 
 interface IFilterBar {
-  authors: { [key: string]: string } | null;
-  locations: { [key: string]: string } | null;
-  searchValue: string;
-  selectedLocation: string;
-  selectedAuthor: string;
-  selectedFrom: string;
-  selectedBefore: string;
-  setSearchValue: Dispatch<SetStateAction<string>>;
-  setSelectedLocation: Dispatch<SetStateAction<string>>;
-  setSelectedAuthor: Dispatch<SetStateAction<string>>;
-  setSelectedFrom: Dispatch<SetStateAction<string>>;
-  setSelectedBefore: Dispatch<SetStateAction<string>>;
-  getData: (e: KeyboardEvent<HTMLInputElement>) => void;
+  appcontext: Context<IAppContext>;
 }
 
-const FilterBar = ({
-  getData,
-  authors,
-  locations,
-  searchValue,
-  selectedLocation,
-  selectedAuthor,
-  selectedFrom,
-  selectedBefore,
-  setSearchValue,
-  setSelectedLocation,
-  setSelectedAuthor,
-  setSelectedFrom,
-  setSelectedBefore,
-}: IFilterBar) => {
-  const handleSearchRequest = (e: KeyboardEvent<HTMLInputElement>) => {
-    if (e.key === 'Enter') {
-      getData(e);
-    }
-  };
+const FilterBar = ({ appcontext }: IFilterBar) => {
+  const {
+    searchActions: { setSearchValue },
+    searchState: { searchValue },
+  } = useContext(appcontext);
 
   return (
     <section>
       <h2 className="visually-hidden">Filter bar</h2>
       <div css={f.filterBarWrap}>
         <div css={f.filterBar}>
-          <SearchField request={handleSearchRequest} searchValue={searchValue} setSearchValue={setSearchValue} />
-          <SelectField name="Author" options={authors} selected={selectedAuthor} setSelected={setSelectedAuthor} />
-          <SelectField
-            name="Location"
-            options={locations}
-            selected={selectedLocation}
-            setSelected={setSelectedLocation}
-          />
-          <RangeField
-            selectedFrom={selectedFrom}
-            selectedBefore={selectedBefore}
-            setSelectedFrom={setSelectedFrom}
-            setSelectedBefore={setSelectedBefore}
-          />
+          <SearchField appcontext={appcontext} searchValue={searchValue} setSearchValue={setSearchValue} />
+          <SelectField appcontext={appcontext} name="Author" />
+          <SelectField appcontext={appcontext} name="Location" />
+          <RangeField appcontext={appcontext} />
         </div>
       </div>
     </section>
