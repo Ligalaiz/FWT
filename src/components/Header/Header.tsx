e05eade5dccecd844b1271283558fb21462588ee
Ligalaiz <slavakuz88@gmail.com>
2022-07-17 20:12:17 +0300

    refactor: AG-4 add background for scroll event

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as h from './Header.style';
 import { SvgSprite } from '@components/SvgSprite';
 
 const Header = () => {
+  const [offset, setOffset] = useState(0);
+
+  window.addEventListener('scroll', () => {
+    setOffset(window.pageYOffset);
+  });
+
   return (
-    <header css={h.header}>
+    <header css={h.header} className={offset > 50 ? 'active' : ''}>
       <div css={h.headerContainer}>
         <div css={h.headerLogo}>
           <a css={h.headerLink} href="#top">
